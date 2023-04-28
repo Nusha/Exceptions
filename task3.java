01dd49d57611f0cd5e850d84ef6281a1604156cc
Ivan Balabay <i.balabai@gmail.com>
2023-04-28 18:19:42 +0300

    task 3 has been completed

---
+++ task3.java
@@ -0,0 +1,27 @@
+
+import java.util.ArrayList;
+
+public class task3 {
+    public static ArrayList<Integer> diffAr(Integer[] arr1, Integer[] arr2) {
+        try {
+            if (arr2.length != arr1.length) {
+                throw new RuntimeException("Длинна одного массива больше другого");
+            }
+        } catch (NullPointerException e) {
+            throw new NullPointerException("Массив не может быть равен NULL");
+        }
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < arr1.length; i++) {
+            result.add(arr1[i] - arr2[i]);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Integer[] a1 = new Integer[]{9, 8, 7, 6, 5, 4, 3};
+        Integer[] a2 = new Integer[]{3, 4, 5, 6, 7, 8};
+
+        System.out.println(diffAr(a1, a2));
+    }
+}
+
