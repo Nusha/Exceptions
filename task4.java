657c8b85811d7650b71e37bccee453f7cb0e0d28
Ivan Balabay <i.balabai@gmail.com>
2023-04-28 18:40:38 +0300

    task 4 has been completed

---
+++ task4.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class task4 {
+    public static ArrayList<Integer> divArr(Integer[] arr1, Integer[] arr2) {
+        if (arr1 == null || arr2 == null) {
+            throw new RuntimeException("Массив не может быть равен NULL");
+        }
+
+        if (arr2.length != arr1.length) {
+            throw new RuntimeException("Длинна одного массива больше другого");
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr2[i] == 0) throw new RuntimeException("Ошибка: деление на ноль");
+            result.add(arr1[i] / arr2[i]);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Integer[] a1 = new Integer[]{9, 8, 7, 6, 5, 4, 3};
+        Integer[] a2 = new Integer[]{3, 4, 5, 1, 7, 8, 1};
+
+        System.out.println(divArr(a1, a2));
+    }
+}
+
